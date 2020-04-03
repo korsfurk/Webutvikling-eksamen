34aca3ae04eda360a02ddb4b3e8e4317fbf35805
korsfurk <42968573+korsfurk@users.noreply.github.com>
2020-04-03 14:22:33 +0200

    Changes

--- pizza/src/App.vue
+++ pizza/src/App.vue
@@ -1,12 +1,27 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Kunde</router-link> |
-      <router-link to="/about">Admin</router-link>
+      <router-link to="/customer">Kunde</router-link> |
+      <router-link to="/admin">Admin</router-link>
+      <h1> Pizza Q U A R A N T I N E</h1>
+      <p>Only takeaway today</p>
     </div>
     <router-view/>
   </div>
 </template>
+
+<script>
+export default {
+  
+}
+
+</script>
+
+
+
+
+
+
 
 <style>
 
