b9df9bca950932db1ea47d402c7fbcfe765c22b0
korsfurk <42968573+korsfurk@users.noreply.github.com>
2020-04-06 20:23:42 +0200

    grid

--- pizza/src/views/Home.vue
+++ pizza/src/views/Home.vue
@@ -1,24 +1,51 @@
 <template>
- <div class="Kunde">
-    <h1>Velkommen</h1>
-    <p></p>
-  </div>
+  <v-layout>
+    <v-flex xs12 sm6 offset-sm3>
+      <v-card>
+        <v-img
+          src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+          height="200px"
+        >
+        </v-img>
+
+        <v-card-title primary-title>
+          <div>
+            <div class="headline">Pizza Corona</div>
+            <span class="grey--text">Den beste pizzaen du noen gang kommer til å smake. Tynn skorpe, masse digg og ekstra sterk!</span>
+          </div>
+        </v-card-title>
+        </v-card>
+
+         <v-card display grid>
+        <v-img
+          src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+          height="200px"
+        >
+        </v-img>
+
+        <v-card-title primary-title>
+          <div>
+            <div class="headline">Pizza Covid</div>
+            <span class="grey--text">Den beste pizzaen du noen gang kommer til å smake. Tynn skorpe, masse digg og ekstra sterk!</span>
+          </div>
+        </v-card-title>
+        </v-card>
+
+        <v-slide-y-transition>
+          <v-card-text v-show="show">
+            
+          </v-card-text>
+        </v-slide-y-transition>
+      
+    </v-flex>
+  </v-layout>
 
   
 </template>
 
-<script>
-// @ is an alias to /src
 
 
 
-</script>
 
-<style scoped>
-h1{
-  font-family: Arial, Helvetica, sans-serif;
-  font-size: 30px;
-  
 
-}
-</style>
+
