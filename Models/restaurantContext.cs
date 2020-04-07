b4fbb7102be7652cad2f859bdf134b300d1334a3
norabergseth <42612724+norabergseth@users.noreply.github.com>
2020-04-07 15:13:32 +0200

    Update restaurantContext.cs

--- Models/restaurantContext.cs
+++ Models/restaurantContext.cs
@@ -4,7 +4,6 @@ namespace EksamenApi.Models{
     public class restaurantContext : DbContext {
 
         public restaurantContext(DbContextOptions<restaurantContext> options):base(options){}
-        public DbSet<EksamenApi.Models.menu> Menu { get; set; }
         public DbSet<EksamenApi.Models.drink> Drink { get; set; }
         public DbSet<EksamenApi.Models.food> Food { get; set; }
         public DbSet<EksamenApi.Models.customer> Customer { get; set; }
