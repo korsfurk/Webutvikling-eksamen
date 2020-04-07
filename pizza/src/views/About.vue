f42f39ddbf45ba50dc3fb60c40845cd4c283093c
korsfurk <42968573+korsfurk@users.noreply.github.com>
2020-04-07 15:15:01 +0200

    update

--- pizza/src/views/About.vue
+++ pizza/src/views/About.vue
@@ -18,4 +18,27 @@ h1{
 }
 </style>
 
- 
+ <!--<template>
+ <div class="Kunde">
+    <h1>Velkommen</h1>
+    <p></p>
+  </div>
+
+  
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+
+</script>
+
+<style scoped>
+h1{
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 30px;
+  
+
+}
+</style>
