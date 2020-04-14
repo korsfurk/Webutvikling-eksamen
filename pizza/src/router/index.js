6452f2ab93e2fa3f2129f3be9a8b46b877e6711d
norabergseth <42612724+norabergseth@users.noreply.github.com>
2020-04-14 12:49:46 +0200

    back to about

--- pizza/src/router/index.js
+++ pizza/src/router/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 
-
 Vue.use(VueRouter)
 
 const routes = [
@@ -12,12 +11,12 @@ const routes = [
     component: Home
   },
   {
-    path: '/admin',
-    name: 'Admin',
+    path: '/about',
+    name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "admin" */ '../views/Admin.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
 ]
 
--- pizza/src/views/About.vue
+++ pizza/src/views/About.vue
@@ -6,9 +6,8 @@
 
 
 <script>
-
 export default {
-  name: "Admin"
+  
 }
 </script>
 
