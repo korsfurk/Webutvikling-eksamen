31aa36ae36ca16df6d30b89b01802e781d1dc501
norabergseth <42612724+norabergseth@users.noreply.github.com>
2020-04-14 14:25:22 +0200

    footer

---
+++ pizza/src/layout/TheFooter.vue
@@ -0,0 +1,39 @@
+<template>
+    <footer>
+        <section>
+            <h3>Contact Info</h3>
+            <ul>
+                <li 
+                    v-for="(value, name, index) in contactInfo"
+                    v-bind:key="index">
+                    {{name}} : {{value}}
+                </li>
+            </ul>
+        </section>
+        <section>
+            <h3>Privacy Policy</h3>
+            <ul>
+                <li></li>
+            </ul>
+        </section>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: "TheFooter",
+    data(){
+        return {
+            contactInfo: {
+                name: "Pizza Q U A R A N T I N E",
+                type: "Takeaway pizza restaurant",
+                phoneNumber: "661 34 567",
+                email: "order@quarantine.no"
+            },
+            privacyPolicy: {
+                name: "Read more here.."
+            }
+        }
+    }
+}
+</script>
--- pizza/src/views/Home.vue
+++ pizza/src/views/Home.vue
@@ -2,17 +2,20 @@
 <template>
 <v-app>
     <TheHeader/>
+    <TheFooter/>
   </v-app>
 </template>
 
 <script>
 
 import TheHeader from "../src/layout/TheHeader"
+import TheFooter from "..src/layout/TheFooter"
 
 export default {
   name: "App",
   components: {
-    TheHeader
+    TheHeader,
+    TheFooter
   }
 }
 
