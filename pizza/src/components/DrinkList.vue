34ab9b1e8b39d6bc316c0fd25b68adac99f84ac6
norabergseth <42612724+norabergseth@users.noreply.github.com>
2020-04-15 19:17:11 +0200

    Update DrinkList.vue

---
+++ pizza/src/components/DrinkList.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <v-row>
+            <v-col v-for="drink in drinks" :key="drink.DrinkId">
+                <DrinkItem 
+                    :DrinkId="drink.DrinkId"
+                    :DrinkName="drink.DrinkName"
+                    :DrinkImageSrc="drink.DrinkImageSrc"
+                    />
+            </v-col>
+        </v-row>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "DrinkList",
+    data(){
+        return {
+            drinks: [ { DrinkId: 999, DrinkName: "TESTFANTA", DrinkImageSrc: "fanta.jpg"}]
+        }
+    }
+}
+</script>
