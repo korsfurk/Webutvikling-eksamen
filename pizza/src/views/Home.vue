5771f63492cfbfc39e27c2a93fca61b18c6d0865
norabergseth <42612724+norabergseth@users.noreply.github.com>
2020-04-16 11:46:26 +0200

    Update Home.vue

--- pizza/src/views/Home.vue
+++ pizza/src/views/Home.vue
@@ -1,35 +1,5 @@
 
 <template>
-
-  <v-layout>
-    <v-flex xs12 sm6 offset-sm3>
-      <v-card>
-        <v-img
-          class="white--text"
-          height="200px"
-          src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
-        >
-          <v-container fill-height fluid>
-            <v-layout fill-height>
-              <v-flex xs12 align-end flexbox>
-                <span class="headline">Pizza Covid</span>
-              </v-flex>
-            </v-layout>
-          </v-container>
-        </v-img>
-        <v-card-title>
-          <div>
-            <span class="grey--text">Number 1</span><br>
-            <span>The classic ham and cheese pizza.</span>
-          </div>
-        </v-card-title>
-        <v-card-actions>
-          <v-btn flat color="#B4D7C2">Bestill</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-  </v-layout>
-
   <div>
     <TheHeader/>
 
