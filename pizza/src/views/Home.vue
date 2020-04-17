d4ce99642b540b47b328e0b6619b38dfc94c8826
norabergseth <42612724+norabergseth@users.noreply.github.com>
2020-04-17 11:55:49 +0200

    Update Home.vue

--- pizza/src/views/Home.vue
+++ pizza/src/views/Home.vue
@@ -14,21 +14,15 @@
 
 <script>
 
-import TheHeader from "../src/layout/TheHeader"
-import DrinkList from "@/components/DrinkList"
-import TheFooter from "../src/layout/TheFooter"
 
 
 export default {
   name: "App",
   components: {
-    TheHeader,
-    DrinkList,
-    TheFooter
     
   }
 }
-
+</script> 
 
 
 
