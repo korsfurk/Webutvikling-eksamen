7c3eaa3a63a2b0077e1e8a30564e1654f96917ff
norabergseth <42612724+norabergseth@users.noreply.github.com>
2020-04-19 21:50:12 +0200

    update

---
+++ pizza/src/components/AddPizzaItem.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <v-row>
+            <v-col cols="12" sm="6" lg="4" class="mx-auto">
+                <v-text-field label="Name"></v-text-field>
+                <v-text-field label="Price"></v-text-field>
+                <v-text-field label="Description"></v-text-field>
+                <v-file-input show-size label="Image"></v-file-input>
+            </v-col>
+        </v-row>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "AddMenuItem",
+    data(){
+        return {
+            newPizza
+        }
+    }
+}
+</script>
--- pizza/src/views/Admin.vue
+++ pizza/src/views/Admin.vue
@@ -1,8 +1,26 @@
 <template>
-  <h1>Add, edit or delete an item</h1>
-  
+<div>
+  <h1>Add new item to menu:</h1>
+  <AddPizzaItem />
+</div>
 </template>
 
+<script>
+import AddPizzaItem from '@/components/AddMenuItem'
 
+export default {
+  name: "Admin",
+  components: {
+    AddPizzaItem
+  }
+}
+</script>
+
+<style scoped>
+h1{
+  text-align: center;
+  padding-top: 30px;
+}
+</style>
 
 
