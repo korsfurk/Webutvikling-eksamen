4593f31f63fec4c04adf288349e8cf074888aa75
norabergseth <42612724+norabergseth@users.noreply.github.com>
2020-04-19 20:20:38 +0200

    update

--- pizza/src/views/Admin.vue
+++ pizza/src/views/Admin.vue
@@ -1,23 +1,8 @@
 <template>
   <h1>Add, edit or delete an item</h1>
-  <MenuForm />
+  
 </template>
 
 
-<script>
-import PizzaForm from '@/components/MenuForm'
 
-export default {
-  name: "Admin",
-  components: {
-    MenuForm
-  }
-}
-</script>
-
-
-<style scoped>
-  
-  
-</style>
 
