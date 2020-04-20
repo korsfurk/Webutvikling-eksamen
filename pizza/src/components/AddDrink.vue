6c2999dd5ff0624761649e7c08cb5476044b6a41
norabergseth <42612724+norabergseth@users.noreply.github.com>
2020-04-20 14:05:21 +0200

    update

--- pizza/src/components/AddDrink.vue
+++ pizza/src/components/AddDrink.vue
@@ -13,7 +13,7 @@
 </template>
 
 <script>
-import axios from 'axios'
+//import axios from 'axios'
 
 export default {
     name: "AddDrink",
@@ -26,7 +26,9 @@ export default {
     },
     methods: {
         postDrink(){
-            this.newDrink.drinkImageSrc = this.file.name;
+            console.log(newDrink);
+            console.log(file);
+            /*this.newDrink.drinkImageSrc = this.file.name;
 
             let data = new FormData();
             data.append("file", this.file);
@@ -41,7 +43,7 @@ export default {
                     data: data,
                     config: { headers: { 'Content-Type' : 'multipart/form-data' } }
                 })
-            })
+            })*/
         }
     }
     
